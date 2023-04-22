a7e1eb1b0b2e7d91866615ce5c4ca37c49f3f691
Gabriel Santos <77673388+GansoHub@users.noreply.github.com>
2023-04-21 23:05:12 -0300

    rvrf

--- mips.v
+++ mips.v
@@ -11,7 +11,14 @@ wire [31:0] aluresult;
 wire zero_flag;
 wire [4:0] shamt;
 wire [31:0] instruction;
-
+input wire [4:0] ReadAddr1;
+input wire [4:0] ReadAddr2;
+output reg [31:0] ReadData1;
+output reg [31:0] ReadData2;
+input wire [4:0] WriteAddr;
+input wire [31:0] WriteData;
+input wire RegWrite;
+input wire Reset;
 wire branch_eq, branch_ne;
 wire memread, memwrite, memtoreg;
 wire regdst, regwrite, alusrc;
